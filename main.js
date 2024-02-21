d2bc9ab200a5b7adad53de4008e76baba0ffb5dd
Kimber Mendoza <kmendoza.prsvr@gmail.com>
2024-02-21 10:23:04 -0600

    Complete

--- main.js
+++ main.js
@@ -7,68 +7,74 @@ function isItLong(string) {
   }
 }
 
-// function isItMedium(string) {
-//   if(string.length > 10  && string.length <= 20){
-// }
+function isItMedium(string) {
+  if (string.length >= 10 && string.length <= 20) {
+    return "That's a regular sized string!";
+  }
+}
 
 function isItShort(string) {
   if (string.length < 10) {
-    return "That's a small string.";
+    return "That's a small string!";
   }
-  else (string.length >= 10); {
+  else {
     return "That's not a small string!";
   }
 }
 
-function  howLongIsMyString(string) {
-  if(string.length > 20) {
+function howLongIsMyString(string) {
+  if (string.length > 20) {
     return "That's a long string!";
   }
-  if(string.length >=10 && string.length <= 20) {
+  if (string.length >= 10 && string.length <= 20) {
     return "That's a regular sized string!";
   }
-  if(string.length < 10) {
+  if (string.length < 10) {
     return "That's a small string!";
   }
 }
 
-function personsHeight(instructorsHeight) {
-instructorsHeight = {
-  "Shaq": 85,
-  "Zooey": 65,
-}
-}
-/**********************************
- *      OUR CODE GOES BELOW.      *
- * THIS IS FOR INTERNAL USE ONLY. *
- * DO NOT ADD OR CHANGE ANYTHING! *
- **********************************/
+function personsHeight(instructor) {
+  if (instructor == "Shaq"){
+    return 85;
+  }
+  if (instructor == "Zooey"){
+    return 65;
+  }
+return "I don't know everyone's height!";
+  }
 
-if (typeof isItLong === 'undefined') {
-  isItLong = undefined;
-}
+  /**********************************
+   *      OUR CODE GOES BELOW.      *
+   * THIS IS FOR INTERNAL USE ONLY. *
+   * DO NOT ADD OR CHANGE ANYTHING! *
+   **********************************/
 
-if (typeof isItMedium === 'undefined') {
-  isItMedium = undefined;
-}
+  if (typeof isItLong === 'undefined') {
+    isItLong = undefined;
+  }
 
-if (typeof isItShort === 'undefined') {
-  isItShort = undefined;
-}
+  if (typeof isItMedium === 'undefined') {
+    isItMedium = undefined;
+  }
 
-if (typeof howLongIsMyString === 'undefined') {
-  howLongIsMyString = undefined;
-}
+  if (typeof isItShort === 'undefined') {
+    isItShort = undefined;
+  }
 
-if (typeof personsHeight === 'undefined') {
-  personsHeight = undefined;
-}
+  if (typeof howLongIsMyString === 'undefined') {
+    howLongIsMyString = undefined;
+  }
+
+  if (typeof personsHeight === 'undefined') {
+    personsHeight = undefined;
+  }
 
 
-module.exports = {
-  isItLong,
-  isItMedium,
-  isItShort,
-  howLongIsMyString,
-  personsHeight,
-}
+  module.exports = {
+    isItLong,
+    isItMedium,
+    isItShort,
+    howLongIsMyString,
+    personsHeight,
+  }
